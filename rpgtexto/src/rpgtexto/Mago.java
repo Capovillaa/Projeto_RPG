af45274f28b39799b150ff93d8b56b3bd2483af9
HenriqueMartins2502 <henrique.dgam@puccampinas.edu.br>
2025-10-28 22:03:34 -0300

    Criacao do metodo usarPoderEspecial()

--- rpgtexto/src/rpgtexto/Mago.java
+++ rpgtexto/src/rpgtexto/Mago.java
@@ -16,5 +16,19 @@ public class Mago extends Personagem {
     public String toString() {
         return "Classe: Mago | " + super.toString();
     }
+
+    @Override
+    public void usarPoderEspecial(Personagem alvo) {
+        System.out.println("---------------------------------");
+        System.out.println(this.getNome() + " conjura um Relâmpago Potencializado (PODER ESPECIAL)!");
+
+        int danoBase = this.getAtaque();
+        int danoFixo = danoBase * 3;
+
+        alvo.receberDano(danoFixo);
+
+        System.out.println(" IMPACTO MÁXIMO! O feitiço atingiu " + alvo.getNome() + " com dano garantido de " + danoFixo + "!");
+        System.out.println("---------------------------------");
+    }
 }
 
